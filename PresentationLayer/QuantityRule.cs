b63e3f4b159f87589a3cf148b60559428c31c793
Mateusz Buda <mateusz@narfsoftware.pl>
2013-11-04 17:02:36 +0100

    QuantityRule class for validation

---
+++ PresentationLayer/QuantityRule.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PresentationLayer
+{
+    public class QuantityRule : ValidationRule
+    {
+        public int Min { get; set; }
+
+        public override ValidationResult Validate(object value,
+                                                   CultureInfo cultureInfo)
+        {
+            int parameter = 0;
+
+            try
+            {
+                if (((string)value).Length > 0)
+                {
+                    parameter = int.Parse((String)value);
+                }
+            }
+            catch (Exception e)
+            {
+                return new ValidationResult(false, "Wprowadź liczbę całkowitą większą od " + Min.ToString() + ".");
+            }
+
+            if ((parameter < this.Min))
+            {
+                return new ValidationResult(false,
+                    "Wprowadź liczbę całkowitą większą od " + Min.ToString() + ".");
+            }
+            return new ValidationResult(true, null);
+        }
+    }
+}
